9e1f4ab2275c9bfb55e6b91cc87411df3afa9a18
Manik patra <72035800+patramanik@users.noreply.github.com>
2024-06-03 14:43:53 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const authRoute = require("./auth");
 const categoryRoute = require("./category");
 const personalDetails = require("./personalDetails");
 const fileRoute = require("./file");
+const homeRoute = require("./homeRoute");
 
-module.exports = { authRoute, categoryRoute, personalDetails, fileRoute };
+module.exports = { authRoute, categoryRoute, personalDetails, fileRoute,homeRoute };
